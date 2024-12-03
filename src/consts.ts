1d6d09bdc9de4f744704a588e6764086dc49dd1e
Marcel Overdijk <marceloverdijk@gmail.com>
2024-12-03 23:04:43 +0100

    Initial website setup

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Site
+export const SITE_NAME = 'bsky4j';
+export const SITE_TITLE = 'bsky4j';
+
+// Socials
+export const BLUESKY_URL = 'https://bsky.app/profile/bsky4j.org';
+export const TWITTER_URL = 'https://x.com/bsky4j';
+export const GITHUB_URL = 'https://github.com/atproto4j/bsky4j';
